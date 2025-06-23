c2a6f4cfc8221cab5c1c0169400aef6b1add2819
Mariah Freire <mahbackesf@gmail.com>
2025-06-22 21:45:27 -0300

    fix: adjust palette options in theme.tsx

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -12,7 +12,13 @@ declare module '@mui/material/styles' {
   }
 
   interface SimplePaletteColorOptions {
-    darker?: string;
+    100?: string;
+    200?: string;
+    300?: string;
+    400?: string;
+    500?: string;
+    600?: string;
+    700?: string;
   }
 
   interface Palette {
