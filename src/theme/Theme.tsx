1e806e5e76b92642331b32af9609cbbe9816fa2c
Mariah Freire <mahbackesf@gmail.com>
2025-06-22 22:05:34 -0300

    fix: remove paletteColorOprtio

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -2,16 +2,13 @@ import { createTheme } from '@mui/material/styles';
 
 declare module '@mui/material/styles' {
   interface PaletteColor {
+    100?: string;
     200?: string;
     300?: string;
     400?: string;
     500?: string;
     600?: string;
     700?: string;
-  }
-
-  interface SimplePaletteColorOptions {
-    darker?: string;
   }
 
   interface Palette {
